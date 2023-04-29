f62deaebcb50a87741335e39d0389cdc3968d259
harryvnguyen <71202025+hvnguyen4@users.noreply.github.com>
2023-04-29 18:02:44 -0500

    first push

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function testFunction()
+{
+    fetch('data.json', { mode: 'no-cors' })
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(text) {
+            var jsonData = JSON.parse(text);
+            console.log(jsonData);
+        })
+        .catch(function(error) {
+            console.log('Error:', error);
+        });
+}
 
+testFunction();
---
+++ something.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+url = "https://api-nba-v1.p.rapidapi.com/teams"
+
+playerMap={}
+
+headers = {
+	"content-type": "application/octet-stream",
+	"X-RapidAPI-Key": "f5513e7d85msh4a88fc5c9f1ed74p1c6ca4jsn3faaeefc49c9",
+	"X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
+}
+
+allTeams = (requests.get(url, headers=headers)).json()["response"]
+
+count=0
+for i in allTeams:
+    if i["nbaFranchise"] and i["name"]!="Home Team Stephen A":
+        count+=1
+        url = "https://api-nba-v1.p.rapidapi.com/players"
+
+        querystring = {"team":i["id"],"season":"2022"}
+
+        headers = {
+            "content-type": "application/octet-stream",
+            "X-RapidAPI-Key": "f5513e7d85msh4a88fc5c9f1ed74p1c6ca4jsn3faaeefc49c9",
+            "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com"
+        }
+
+        playersInTeam = (requests.get(url, headers=headers, params=querystring)).json()["response"]
+
+        for j in playersInTeam:
+            playerMap[j["firstname"]+" "+j["lastname"]]=j["id"]
+
+json_data=json.dumps(playerMap)
+
+with open('data.json', 'w') as f:
+    f.write(json_data)
